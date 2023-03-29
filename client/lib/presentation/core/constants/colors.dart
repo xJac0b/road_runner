9b96692432424f2e28235e9d0cf5e1ea042e93ba
xJac0b <kubap1402@gmail.com>
2023-03-29 11:00:53 +0200

    update color

--- client/lib/presentation/core/constants/colors.dart
+++ client/lib/presentation/core/constants/colors.dart
@@ -5,5 +5,5 @@ const primaryVariant = Color(0xff0097A7);
 const secondary = Color(0xff26A69A);
 const secondaryVariant = Color(0xff00796B);
 const background = Color(0xffffffff);
-const surface = Color(0xffffffff);
+const surface = Color.fromARGB(255, 236, 236, 236);
 const error = Color(0xffb00020);
